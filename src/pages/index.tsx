72c3d8a8d813c5b63507d197d5314b2c934bdeff
Jordon <jordontgarcia@gmail.com>
2022-02-02 08:03:50 -0800

    Added CSS text selection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import Stats from '../components/Stats'
+import Insight from '../components/Insight'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-white">
+    <div className="bg-white selection:bg-purple-700 selection:text-black">
       <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
         <div className="text-center">
-          <h1 className="text-base font-semibold text-indigo-600 tracking-wide uppercase">Trice Presents</h1>
+          <h1 className="text-lg font-semibold text-indigo-600 tracking-wide uppercase">Trice Presents</h1>
           <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-            Trice 3 Public Release
+            Trice<span className="text-indigo-500 selection:text-white"> V3 </span>Public Release
           </p>
           <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
             Trice&apos;s system-wide software upgrade is finally here.
@@ -19,6 +20,7 @@ const Home: NextPage = () => {
 
       {/* Import and render components */}
       <Stats />
+      <Insight />
 
     </div>
   )
