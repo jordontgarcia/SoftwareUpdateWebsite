78da37fa6473da05781b9fc5852300ae6cdc43c6
Jordon <jordontgarcia@gmail.com>
2022-02-03 00:36:19 -0800

    Updated Stats Section

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -3,12 +3,12 @@ import { ArrowSmDownIcon, ArrowSmUpIcon } from '@heroicons/react/solid'
 import type { NextPage } from 'next'
 
 const stats = [
-    { name: 'Avg. Build Time', stat: '362ms', previousStat: '7.4s', change: '20x Faster build time', changeType: 'increase', changeText: 'faster' },
+    { name: 'Avg. Build Time', stat: '362ms', previousStat: '7.4s', change: '20x Faster builds', changeType: 'increase', changeText: 'faster' },
     { name: 'Package Size', stat: '64mb', previousStat: '556mb', change: '8x Smaller file size', changeType: 'decrease' },
     { name: 'Resource Usage', stat: '92mb', previousStat: '381mb', change: 'Uses 4x less RAM', changeType: 'decrease' },
-    { name: 'Avg. Write Speeds ', stat: '1.4GB/s', previousStat: '108MB/s', change: 'Supports speeds 13x faster', changeType: 'increase' },
-    { name: 'Userbase Compatibility Support', stat: '97.52%', previousStat: '62.11%', change: '8 Million users supported', changeType: 'increase' },
-    { name: 'Max Supported Download Speeds', stat: '1.3GB/s', previousStat: '127MS/s', change: '10x Faster download speeds', changeType: 'increase' },
+    { name: 'Avg. Write Speeds ', stat: '1.4GB/s', previousStat: '108MB/s', change: '13x Faster support', changeType: 'increase' },
+    { name: 'Userbase Compatibility Support', stat: '97.52%', previousStat: '62.11%', change: '8 Million supported', changeType: 'increase' },
+    { name: 'Max Supported Download Speeds', stat: '1.3GB/s', previousStat: '127MS/s', change: '10x Faster downloads', changeType: 'increase' },
 ]
 
 function classNames(...classes) {
@@ -17,11 +17,18 @@ function classNames(...classes) {
 
 const Stats: NextPage = () => {
     return (
-        <div className="bg-white selection:bg-purple-700 selection:text-black">
-            <div className="md:container md:mx-auto">
-                <div className="pb-5 border-b border-gray-200">
-                    <h3 className="text-lg leading-6 font-medium text-gray-900">What we&apos;ve accomplished:</h3>
+        <div className="bg-white selection:bg-purple-500 selection:text-black">
+            <div className="md:container md:max-w-screen-xl md:mx-auto">
+
+                <div className="relative">
+                    <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                        <div className="w-full border-t border-gray-300" />
+                    </div>
+                    <div className="relative flex justify-center">
+                        <span className="px-3 bg-white text-lg font-medium text-gray-900">Stats speak for us, what we&apos;ve accomplished:</span>
+                    </div>
                 </div>
+
                 <dl className="mt-5 grid grid-cols-1 rounded-lg bg-white overflow-hidden shadow divide-y divide-gray-200 md:grid-cols-3 md:divide-y-0 md:divide-x">
                     {stats.map((item) => (
                         <div key={item.name} className="px-4 py-5 sm:p-6">
