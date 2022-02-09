d9ed12e2b403e60721f535352e1e7a9ab1c26c2c
Jordon <jordontgarcia@gmail.com>
2022-02-09 05:19:51 -0800

    Changed Colors to Support Dark Mode

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,6 +1,5 @@
 import { ChevronDownIcon } from '@heroicons/react/outline'
 import { Disclosure } from '@headlessui/react'
-import type { NextPage } from 'next'
 
 const faqs = [
 	{
@@ -23,22 +22,26 @@ function classNames(...classes) {
 	return classes.filter(Boolean).join(' ');
 }
 
-const FAQ: NextPage = () => {
+const FAQ: React.FunctionComponent = () => {
 	return (
-		<div className='bg-white'>
+		<div className='bg-white dark:bg-transparent'>
 			{/* Section Line Separator */}
-			<div className='bg-white relative mt-8'>
+			<div className='bg-white dark:bg-transparent relative mt-8'>
 				<div className='absolute inset-0 flex items-center' aria-hidden='true'>
-					<div className='w-full border-t border-gray-300' />
+					<div className='w-full border-t dark:border-none border-gray-300' />
 				</div>
 				<div className='relative flex justify-center'>
-					<span className='px-3 bg-white text-lg font-medium text-gray-900'>Have Questions? We got you!</span>
+					<span className='px-3 bg-white dark:bg-transparent text-lg font-medium dark:text-white text-gray-900'>
+						Have Questions? We got you!
+					</span>
 				</div>
 			</div>
 
 			<div className='max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8'>
 				<div className='max-w-3xl mx-auto divide-y-2 divide-gray-200'>
-					<h2 className='text-center text-3xl font-extrabold text-gray-900 sm:text-4xl'>Frequently asked questions</h2>
+					<h2 className='text-center text-3xl font-extrabold dark:text-gray-200 text-gray-900 sm:text-4xl'>
+						Frequently asked questions
+					</h2>
 					<dl className='mt-6 space-y-6 divide-y divide-gray-200'>
 						{faqs.map((faq) => (
 							<Disclosure as='div' key={faq.question} className='pt-6'>
@@ -46,7 +49,7 @@ const FAQ: NextPage = () => {
 									<>
 										<dt className='text-lg'>
 											<Disclosure.Button className='text-left w-full flex justify-between items-start text-gray-400'>
-												<span className='font-medium text-gray-900'>{faq.question}</span>
+												<span className='font-medium dark:text-gray-200 text-gray-900'>{faq.question}</span>
 												<span className='ml-6 h-7 flex items-center'>
 													<ChevronDownIcon
 														className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform')}
@@ -56,7 +59,7 @@ const FAQ: NextPage = () => {
 											</Disclosure.Button>
 										</dt>
 										<Disclosure.Panel as='dd' className='mt-2 pr-12'>
-											<p className='text-base text-gray-500'>{faq.answer}</p>
+											<p className='text-base dark:text-gray-400 text-gray-500'>{faq.answer}</p>
 										</Disclosure.Panel>
 									</>
 								)}
