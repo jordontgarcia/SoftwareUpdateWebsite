e1cc99afc8e890e3fe44632b9fc5c9994c04fada
Jordon <jordontgarcia@gmail.com>
2022-02-09 05:20:52 -0800

    Added DarkMode Button Toggle

---
+++ src/components/ToggleButton.tsx
@@ -0,0 +1,18 @@
+import { useTheme } from 'next-themes'
+
+const ToggleButton: React.FunctionComponent = () => {
+	const { theme, setTheme } = useTheme();
+	return (
+		<div className='float-right mr-20 mt-4'>
+			<button
+				type='button'
+				className='fixed z-30 select-none text-white inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm bg-toggle-blue bg-black dark:bg-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200'
+				onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+			>
+				{theme === 'light' ? '🌞' : '🌙'}
+			</button>
+		</div>
+	);
+};
+
+export default ToggleButton
