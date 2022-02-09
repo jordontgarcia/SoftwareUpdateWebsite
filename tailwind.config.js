fb886b24d18d396271991a27cf4f30dabd5ca9c3
Jordon <jordontgarcia@gmail.com>
2022-02-09 05:17:21 -0800

    Dark Mode Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,22 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
+        'dark-primary': 'rgb(9, 12, 21)',
+        'dark-secondary': 'rgb(11, 14, 28)',
+        'gray-lead': 'rgb(161, 168, 181)',
         sky: colors.sky,
         teal: colors.teal,
         rose: colors.rose,
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
-}
+  plugins: [require('@tailwindcss/forms')],
+};
