c06775e02e1dfd688fc75452e990f40320aa3caa
Jordon <jordontgarcia@gmail.com>
2022-02-10 20:38:31 -0800

    Updated LightMode UI Colors

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -5,7 +5,7 @@ const faqs = [
 	{
 		question: 'How long will V3 continue to receive support and updates?',
 		answer:
-			'Trice V3 will receive support and updates until 2026. If you have any concerns about migrating to v3, please reach out to our support team.',
+			'Trice V3 will receive support and updates until 2026. If you have any concerns about migrating to V3, please reach out to our support team.',
 	},
 	{
 		question: 'Is V3 compatible with V2?',
@@ -24,7 +24,7 @@ function classNames(...classes) {
 
 const FAQ: React.FunctionComponent = () => {
 	return (
-		<div className='bg-white dark:bg-transparent'>
+		<div className='bg-gray-900 dark:bg-transparent'>
 			{/* Section Line Separator */}
 			<div className='bg-white dark:bg-transparent relative mt-8'>
 				<div className='absolute inset-0 flex items-center' aria-hidden='true'>
@@ -39,7 +39,7 @@ const FAQ: React.FunctionComponent = () => {
 
 			<div className='max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8'>
 				<div className='max-w-3xl mx-auto divide-y-2 divide-gray-200'>
-					<h2 className='text-center text-3xl font-extrabold dark:text-gray-200 text-gray-900 sm:text-4xl'>
+					<h2 className='text-center text-3xl font-extrabold dark:text-gray-200 text-gray-100 sm:text-4xl'>
 						Frequently asked questions
 					</h2>
 					<dl className='mt-6 space-y-6 divide-y divide-gray-200'>
@@ -49,7 +49,7 @@ const FAQ: React.FunctionComponent = () => {
 									<>
 										<dt className='text-lg'>
 											<Disclosure.Button className='text-left w-full flex justify-between items-start text-gray-400'>
-												<span className='font-medium dark:text-gray-200 text-gray-900'>{faq.question}</span>
+												<span className='font-medium dark:text-gray-200 text-gray-100'>{faq.question}</span>
 												<span className='ml-6 h-7 flex items-center'>
 													<ChevronDownIcon
 														className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform')}
@@ -59,7 +59,7 @@ const FAQ: React.FunctionComponent = () => {
 											</Disclosure.Button>
 										</dt>
 										<Disclosure.Panel as='dd' className='mt-2 pr-12'>
-											<p className='text-base dark:text-gray-400 text-gray-500'>{faq.answer}</p>
+											<p className='text-base dark:text-gray-400 text-gray-300'>{faq.answer}</p>
 										</Disclosure.Panel>
 									</>
 								)}
