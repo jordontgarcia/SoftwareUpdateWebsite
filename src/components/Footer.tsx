51867df8b6acdecf6a5d9907b2491b7f9c4f448d
Jordon <jordontgarcia@gmail.com>
2022-02-10 20:38:38 -0800

    Updated LightMode UI Colors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -90,7 +90,7 @@ const navigation = {
   const Footer: React.FunctionComponent = () => {
 	return (
 	  <footer
-		className='bg-white dark:bg-dark-primary'
+		className='bg-dark-lead dark:bg-dark-primary'
 		aria-labelledby='footer-heading'
 	  >
 		<h2 id='footer-heading' className='sr-only'>
@@ -99,7 +99,7 @@ const navigation = {
 		<div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8'>
 		  <div className='xl:grid xl:grid-cols-3 xl:gap-8'>
 			<div className='space-y-8 xl:col-span-1'>
-			  <p className='dark:text-gray-400 text-gray-500 text-base'>
+			  <p className='text-gray-400 text-base'>
 				Trice is honored to provide value and impact through our
 				technologies.
 			  </p>
@@ -110,7 +110,7 @@ const navigation = {
 					target='_blank'
 					rel='noopener noreferrer'
 					key={item.name}
-					className='dark:text-gray-200 dark:hover:text-gray-700 text-gray-400 hover:text-gray-500'
+					className='dark:text-gray-200 dark:hover:text-gray-700 text-gray-300 hover:text-gray-400'
 				  >
 					<span className='sr-only'>{item.name}</span>
 					<item.icon className='h-6 w-6' aria-hidden='true' />
@@ -121,7 +121,7 @@ const navigation = {
 			<div className='mt-12 grid grid-cols-2 gap-8 xl:mt-0 xl:col-span-2'>
 			  <div className='md:grid md:grid-cols-2 md:gap-8'>
 				<div>
-				  <h3 className='text-sm font-semibold dark:text-white text-gray-400 tracking-wider uppercase'>
+				  <h3 className='text-sm font-semibold dark:text-white text-gray-300 tracking-wider uppercase'>
 					Solutions
 				  </h3>
 				  <ul role='list' className='mt-4 space-y-4'>
@@ -129,7 +129,7 @@ const navigation = {
 					  <li key={item.name}>
 						<a
 						  href={item.href}
-						  className='text-base dark:text-gray-400 dark:hover:text-white text-gray-500 hover:text-gray-900'
+						  className='text-base dark:text-gray-400 dark:hover:text-white text-gray-400 hover:text-gray-500'
 						>
 						  {item.name}
 						</a>
@@ -138,7 +138,7 @@ const navigation = {
 				  </ul>
 				</div>
 				<div className='mt-12 md:mt-0'>
-				  <h3 className='text-sm font-semibold dark:text-white text-gray-400 tracking-wider uppercase'>
+				  <h3 className='text-sm font-semibold dark:text-white text-gray-300 tracking-wider uppercase'>
 					Support
 				  </h3>
 				  <ul role='list' className='mt-4 space-y-4'>
@@ -146,7 +146,7 @@ const navigation = {
 					  <li key={item.name}>
 						<a
 						  href={item.href}
-						  className='text-base dark:text-gray-400 dark:hover:text-white text-gray-500 hover:text-gray-900'
+						  className='text-base dark:text-gray-400 dark:hover:text-white text-gray-400 hover:text-gray-500'
 						>
 						  {item.name}
 						</a>
@@ -157,7 +157,7 @@ const navigation = {
 			  </div>
 			  <div className='md:grid md:grid-cols-2 md:gap-8'>
 				<div>
-				  <h3 className='text-sm font-semibold dark:text-white text-gray-400 tracking-wider uppercase'>
+				  <h3 className='text-sm font-semibold dark:text-white text-gray-300 tracking-wider uppercase'>
 					Company
 				  </h3>
 				  <ul role='list' className='mt-4 space-y-4'>
@@ -165,7 +165,7 @@ const navigation = {
 					  <li key={item.name}>
 						<a
 						  href={item.href}
-						  className='text-base dark:text-gray-400 dark:hover:text-white text-gray-500 hover:text-gray-900'
+						  className='text-base dark:text-gray-400 dark:hover:text-white text-gray-400 hover:text-gray-500'
 						>
 						  {item.name}
 						</a>
@@ -174,7 +174,7 @@ const navigation = {
 				  </ul>
 				</div>
 				<div className='mt-12 md:mt-0'>
-				  <h3 className='text-sm font-semibold dark:text-white text-gray-400 tracking-wider uppercase'>
+				  <h3 className='text-sm font-semibold dark:text-white text-gray-300 tracking-wider uppercase'>
 					Legal
 				  </h3>
 				  <ul role='list' className='mt-4 space-y-4'>
@@ -182,7 +182,7 @@ const navigation = {
 					  <li key={item.name}>
 						<a
 						  href={item.href}
-						  className='text-base dark:text-gray-400 dark:hover:text-white text-gray-500 hover:text-gray-900'
+						  className='text-base dark:text-gray-400 dark:hover:text-white text-gray-400 hover:text-gray-500'
 						>
 						  {item.name}
 						</a>
@@ -201,7 +201,7 @@ const navigation = {
 				target='_blank'
 				rel='noopener noreferrer'
 			  >
-				<span className='dark:text-white text-blue-700 font-bold'>
+				<span className='dark:text-white text-blue-500 font-bold'>
 				  {' '}
 				  Next.js{' '}
 				</span>
@@ -212,7 +212,7 @@ const navigation = {
 				target='_blank'
 				rel='noopener noreferrer'
 			  >
-				<span className='dark:text-white text-blue-700 font-bold'>
+				<span className='dark:text-white text-blue-500 font-bold'>
 				  {' '}
 				  TailwindCSS.
 				</span>
