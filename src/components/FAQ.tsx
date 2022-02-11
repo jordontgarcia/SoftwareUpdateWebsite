b0c17431680971b7764b0846f7c1b129f822db99
Jordon <jordontgarcia@gmail.com>
2022-02-11 00:40:17 -0800

    UI Improvements

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -24,52 +24,47 @@ function classNames(...classes) {
 
 const FAQ: React.FunctionComponent = () => {
 	return (
-		<div className='bg-gray-900 dark:bg-transparent'>
-			{/* Section Line Separator */}
-			<div className='bg-white dark:bg-transparent relative mt-8'>
-				<div className='absolute inset-0 flex items-center' aria-hidden='true'>
-					<div className='w-full border-t dark:border-none border-gray-300' />
-				</div>
-				<div className='relative flex justify-center'>
-					<span className='px-3 bg-white dark:bg-transparent text-lg font-medium dark:text-white text-gray-900'>
-						Have Questions? We got you!
-					</span>
-				</div>
-			</div>
-
-			<div className='max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8'>
-				<div className='max-w-3xl mx-auto divide-y-2 divide-gray-200'>
-					<h2 className='text-center text-3xl font-extrabold dark:text-gray-200 text-gray-100 sm:text-4xl'>
-						Frequently asked questions
-					</h2>
-					<dl className='mt-6 space-y-6 divide-y divide-gray-200'>
-						{faqs.map((faq) => (
-							<Disclosure as='div' key={faq.question} className='pt-6'>
-								{({ open }) => (
-									<>
-										<dt className='text-lg'>
-											<Disclosure.Button className='text-left w-full flex justify-between items-start text-gray-400'>
-												<span className='font-medium dark:text-gray-200 text-gray-100'>{faq.question}</span>
-												<span className='ml-6 h-7 flex items-center'>
-													<ChevronDownIcon
-														className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform')}
-														aria-hidden='true'
-													/>
-												</span>
-											</Disclosure.Button>
-										</dt>
-										<Disclosure.Panel as='dd' className='mt-2 pr-12'>
-											<p className='text-base dark:text-gray-400 text-gray-300'>{faq.answer}</p>
-										</Disclosure.Panel>
-									</>
-								)}
-							</Disclosure>
-						))}
-					</dl>
-				</div>
-			</div>
+	  <div className="bg-dark-lead dark:bg-transparent">
+		<div className="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
+		  <div className="max-w-3xl mx-auto divide-y-2 divide-gray-200">
+			<h2 className="text-center text-3xl font-extrabold dark:text-gray-200 text-gray-100 sm:text-4xl">
+			  Frequently asked questions
+			</h2>
+			<dl className="mt-6 space-y-6 divide-y divide-gray-200">
+			  {faqs.map((faq) => (
+				<Disclosure as="div" key={faq.question} className="pt-6">
+				  {({ open }) => (
+					<>
+					  <dt className="text-lg">
+						<Disclosure.Button className="text-left w-full flex justify-between items-start text-gray-400">
+						  <span className="font-medium dark:text-gray-200 text-gray-100">
+							{faq.question}
+						  </span>
+						  <span className="ml-6 h-7 flex items-center">
+							<ChevronDownIcon
+							  className={classNames(
+								open ? '-rotate-180' : 'rotate-0',
+								'h-6 w-6 transform'
+							  )}
+							  aria-hidden="true"
+							/>
+						  </span>
+						</Disclosure.Button>
+					  </dt>
+					  <Disclosure.Panel as="dd" className="mt-2 pr-12">
+						<p className="text-base dark:text-gray-400 text-gray-300">
+						  {faq.answer}
+						</p>
+					  </Disclosure.Panel>
+					</>
+				  )}
+				</Disclosure>
+			  ))}
+			</dl>
+		  </div>
 		</div>
+	  </div>
 	);
-};
-
-export default FAQ
+  };
+  
+  export default FAQ
