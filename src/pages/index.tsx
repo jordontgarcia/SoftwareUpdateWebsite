bd3229b27e21a576dfd93787c0e69a10060ca93c
Jordon <jordontgarcia@gmail.com>
2022-03-12 18:21:16 -0800

    UI/UX Minor Updates, code structure revisions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,22 @@
-import UpdateComponent from '../components/UpdateFlag'
-import IntroSection from '../components/IntroSection'
-import CEOQuote from '../components/CeoQuote'
-import Pricing from '../components/Pricing'
-import Insight from '../components/Insight'
-import Footer from '../components/Footer'
-import Stats from '../components/Stats'
-import Info from '../components/Info'
 import type { NextPage } from 'next'
+
+import IntroSection from '../components/IntroSection'
+import UpdateFlag from '../components/UpdateFlag'
+
+import Stats from '../components/Stats'
+import Insight from '../components/Insight'
+import Pricing from '../components/Pricing'
+import Info from '../components/CoreValues'
+import CEOQuote from '../components/CeoQuote'
+
 import FAQ from '../components/FAQ'
+import Footer from '../components/Footer'
 
 const Home: NextPage = () => {
 	return (
 		<div className='bg-white dark:bg-dark-primary selection:bg-purple-700 dark:selection:bg-yellow-500 selection:text-black'>
 			{/* Render Components */}
-			<UpdateComponent />
+			<UpdateFlag />
 			<IntroSection />
 			<div className='max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8'>
 				<div className='text-center'>
