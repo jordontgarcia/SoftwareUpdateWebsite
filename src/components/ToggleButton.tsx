25fa63e49e26abab78ce647a85588e0e3df2af3a
Jordon <jordontgarcia@gmail.com>
2022-03-12 18:20:27 -0800

    Added more padding to button

--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -6,7 +6,7 @@ const ToggleButton: React.FunctionComponent = () => {
 		<div className='float-right'>
 			<button
 				type='button'
-				className='z-30 select-none text-white inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm bg-toggle-blue bg-dark-lead dark:bg-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200'
+				className='z-30 select-none text-white inline-flex items-center px-4 py-3 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm bg-toggle-blue bg-dark-lead dark:bg-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200'
 				onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
 			>
 				{theme === 'light' ? '☀️' : '🌙'}
