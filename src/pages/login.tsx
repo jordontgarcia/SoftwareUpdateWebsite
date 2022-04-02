74ecd15d5946b20b85fa3ef64bc929032b7e7687
Jordon <jordontgarcia@gmail.com>
2022-04-02 15:58:35 -0700

    Added login page Section, code restructure

---
+++ src/pages/login.tsx
@@ -0,0 +1,176 @@
+/* eslint-disable @next/next/no-img-element */
+import type { NextPage } from 'next';
+
+const Login: NextPage = () => {
+	return (
+		<>
+			<div className='min-h-full h-full flex'>
+				<div className='flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24'>
+					<div className='mx-auto w-full max-w-sm lg:w-96'>
+						<div>
+							<h2 className='mt-6 text-3xl font-extrabold text-gray-900 dark:text-gray-50'>Sign in to Trice</h2>
+						</div>
+
+						<div className='mt-8'>
+							<div>
+								<div>
+									<p className='text-sm font-medium text-gray-700 dark:text-gray-100'>Sign in with</p>
+
+									<div className='mt-1 grid grid-cols-3 gap-3'>
+										<div>
+											<a
+												href='https://instagram.com/jordontgarcia'
+												target="_blank"
+												rel='noreferrer'
+												className='w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50'
+											>
+												<span className='sr-only'>Sign in with Facebook</span>
+												<svg
+													xmlns='http://www.w3.org/2000/svg'
+													fill='currentColor'
+													className='w-5 h-5'
+													viewBox='0 0 16 16'
+												>
+													<path d='M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z' />
+												</svg>
+											</a>
+										</div>
+
+										<div>
+											<a
+												href='https://twitter.com/jordontgarcia'
+												target='_blank'
+												rel='noreferrer'
+												className='w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50'
+											>
+												<span className='sr-only'>Sign in with Twitter</span>
+												<svg className='w-5 h-5' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20'>
+													<path d='M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84' />
+												</svg>
+											</a>
+										</div>
+
+										<div>
+											<a
+												href='http://github.com/jordongarcia'
+												target='_blank'
+												rel='noreferrer'
+												className='w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50'
+											>
+												<span className='sr-only'>Sign in with GitHub</span>
+												<svg className='w-5 h-5' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20'>
+													<path
+														fillRule='evenodd'
+														d='M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z'
+														clipRule='evenodd'
+													/>
+												</svg>
+											</a>
+										</div>
+									</div>
+								</div>
+
+								<div className='mt-6 relative'>
+									<div className='relative flex justify-center text-sm'>
+										<span className='px-2 bg-transparent text-gray-500 dark:text-gray-100'>Or continue with</span>
+									</div>
+								</div>
+							</div>
+
+							<div className='mt-6'>
+								<form action='#' method='POST' className='space-y-6'>
+									<div>
+										<label htmlFor='email' className='block text-sm font-medium text-gray-700 dark:text-gray-100'>
+											Email address
+										</label>
+										<div className='mt-1'>
+											<input
+												id='email'
+												name='email'
+												type='email'
+												placeholder='Trice Email'
+												autoComplete='email'
+												required
+												className='text-gray-600 appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+											/>
+										</div>
+									</div>
+
+									<div className='space-y-1'>
+										<label htmlFor='password' className='block text-sm font-medium text-gray-700 dark:text-gray-100'>
+											Password
+										</label>
+										<div className='mt-1'>
+											<input
+												id='password'
+												name='password'
+												type='password'
+												placeholder='Trice Password'
+												autoComplete='current-password'
+												required
+												className='text-gray-600 appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+											/>
+										</div>
+									</div>
+
+									<div className='flex items-center justify-between'>
+										<div className='flex items-center'>
+											<input
+												id='remember-me'
+												name='remember-me'
+												type='checkbox'
+												className='h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded'
+											/>
+											<label htmlFor='remember-me' className='ml-2 block text-sm text-gray-900 dark:text-gray-100'>
+												Remember me
+											</label>
+										</div>
+
+										<div className='text-sm'>
+											<a href='#' className='font-medium text-blue-600 hover:text-blue-500'>
+												Forgot your password?
+											</a>
+										</div>
+									</div>
+
+									{/* Sign In Button Start */}
+									<div>
+										<button
+											type='submit'
+											className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-700 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600'
+										>
+											Sign me in
+										</button>
+									</div>
+									{/* Sign in Button End */}
+
+									{/* Home Button Start */}
+									<div>
+										<a href='./'>
+											<button
+												type='button'
+												className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-700 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600'
+											>
+												Back To Home
+											</button>
+										</a>
+									</div>
+									{/* Home Button End */}
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className='hidden lg:block relative w-0 flex-1 select-none'>
+					<img
+						className='absolute inset-0 h-full w-full object-cover'
+						src='https://images.unsplash.com/photo-1521464302861-ce943915d1c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80'
+						alt='Golden Gate Bridge'
+					/>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Login;
