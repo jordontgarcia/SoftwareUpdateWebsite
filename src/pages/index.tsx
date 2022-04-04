84262378fdb48643b9821e04258341d51df52ec9
Jordon <jordontgarcia@gmail.com>
2022-04-04 01:54:33 -0700

    Mobile/Responsive UI improvements, minor changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,19 +14,21 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
 	return (
-		<div className='bg-white dark:bg-dark-primary selection:bg-purple-700 dark:selection:bg-yellow-500 selection:text-black'>
+		<div className='bg-white dark:bg-dark-primary'>
 			{/* Render Components */}
 			<UpdateFlag />
 			<IntroSection />
 			<div className='max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8'>
 				<div className='text-center'>
-					<h1 className='text-lg font-semibold dark:text-white text-indigo-700 tracking-wide uppercase'>Trice Presents</h1>
-					<p className='mt-1 text-4xl font-extrabold dark:text-gray-100 text-black sm:text-5xl sm:tracking-tight lg:text-6xl'>
+					<h1 className='text-base laptop:text-lg font-semibold dark:text-white text-indigo-700 tracking-wide uppercase'>
+						Trice Presents
+					</h1>
+					<p className='mt-1 text-3xl laptop:text-6xl font-extrabold dark:text-gray-100 text-black sm:text-5xl sm:tracking-tight'>
 						Trice
 						<span className='dark:text-blue-600 text-blue-700 selection:text-white'> V3 </span>
 						Public Release
 					</p>
-					<p className='max-w-xl mt-5 mx-auto text-xl dark:text-white text-gray-600'>
+					<p className='max-w-xl mt-5 mx-auto text-lg laptop:text-xl dark:text-white text-gray-600'>
 						Trice&apos;s system-wide software upgrade is finally here. Our beta testers made Trice 3 the best it could be, for
 						you.
 					</p>
