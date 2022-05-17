077b3ade988e06af695db392069462ec5749cf5e
Jordon <jordontgarcia@gmail.com>
2022-05-17 16:09:46 -0700

    Fixed Hydration React Issue

--- src/components/Index/ToggleButton.tsx
+++ src/components/Index/ToggleButton.tsx
@@ -1,13 +1,19 @@
 import { useTheme } from 'next-themes';
+import { useState, useEffect } from 'react';
 
 const ToggleButton: React.FunctionComponent = () => {
+	const [mounted, setMounted] = useState(false);
 	const { theme, setTheme } = useTheme();
+
+	useEffect(() => setMounted(true), []);
+	if (!mounted) return null;
+
 	return (
 		<div className='float-right'>
 			<button
 				type='button'
-				className='z-30 select-none text-white inline-flex items-center px-4 py-3 tablet:px-3 tablet:py-2 laptop:px-3 laptop:py-2 border border-transparent text-base leading-4 font-medium rounded-md shadow-sm bg-toggle-blue bg-dark-lead dark:bg-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200'
-				onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+				className='z-30 select-none text-white inline-flex items-center px-4 py-3 tablet:px-3 tablet:py-2 laptop:px-3 laptop:py-2 border border-transparent text-base leading-4 font-medium rounded-md shadow-sm bg-toggle-blue bg-blue-800 dark:bg-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200'
+				onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
 			>
 				{theme === 'light' ? '☀️' : '🌙'}
 			</button>
